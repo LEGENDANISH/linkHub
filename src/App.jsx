db8886249a5531cb26e922291b73ac897a21b2d4
LEGENDANISH <0504anish@gmail.com>
2026-02-06 23:45:52 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex text-3xl font-bold bg-[#d2e823]">
+      
+      <div className="flex flex-col justify-center w-1/2 px-12">
+        
+        {/* Heading */}
+        <div className="text-[#254f1a] text-7xl font-extrabold mb-6">
+          A Link in bio built <br /> for you.
+        </div>
+
+        {/* Text below heading */}
+        <div className="text-[#254f1a] text-lg font-medium max-w-xl">
+          Join 70M+ people using Linktree for their link in bio. One link to help you
+          share everything you create, curate and sell from your Instagram, TikTok,
+          Twitter, YouTube and other social media profiles.
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
